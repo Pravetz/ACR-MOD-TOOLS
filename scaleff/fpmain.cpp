1874b8e07bc1d2546320a03fb67902f72352ce4e
Volodymyr Didur <46265909+Pravetz@users.noreply.github.com>
2025-11-16 21:27:14 +0200

    Add ACR 1.8 utils (scaleff)

---
+++ scaleff/fpmain.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+
+#define MAXMAGNITUDE 2097152
+
+int main(int argc, char **argv){
+	if(argc < 2){
+		return EXIT_SUCCESS;
+	}
+	
+	std::ofstream _file;
+	_file.open(argv[1]);
+	_file <<"	set_variable = {\n"<<
+			"		which = temp_counter\n"<<
+			"		value = $amount$\n"<<
+			"	}\n";
+	for(int i = MAXMAGNITUDE; i > 0; i>>=1){
+		_file <<"	if = {\n"<<
+				"		limit = {\n"<<
+				"			AND = {\n"<<
+				"				check_variable = {\n"<<
+				"					which = temp_counter\n"<<
+				"					value = "<<i<<"\n"<<
+				"				}\n"<<
+				"			}\n"<<
+				"		}\n"<<
+				"		$action$ = "<<((double)i / 1000.0)<<"\n"<<
+				"		subtract_variable = {\n"<<
+				"			which = temp_counter\n"<<
+				"			value = "<<i<<"\n"<<
+				"		}\n"<<
+				"	}\n";
+	}
+	
+	return EXIT_SUCCESS;
+}
