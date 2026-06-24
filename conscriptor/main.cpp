3931e00915ebe8fea579ffbfe11e259a0f4b537c
Volodymyr Didur <46265909+Pravetz@users.noreply.github.com>
2026-06-24 09:35:41 +0300

    ACR 2.2.0 versions of utils

--- conscriptor/main.cpp
+++ conscriptor/main.cpp
@@ -14,6 +14,7 @@ int main(int argc, char **argv){
 	std::ofstream _landf;
 	std::ofstream _landfl;
 	std::ofstream _landfm;
+	std::ofstream _landfpm;
 	std::ofstream _landfml;
 	_landf.open(land_levies.c_str());
 	_landfl.open(land_levies_l.c_str());
@@ -24,6 +25,7 @@ int main(int argc, char **argv){
 	constexpr double g_step = 2.55; 
 	
 	constexpr float unrest_modifier = 0.5f / 50.0f;
+	constexpr float purist_manpower_recovery = 0.5f / 50.0f;
 	constexpr float war_exhaustion_cost_impact = 0.01f;
 	
 	if(_landf){
@@ -35,10 +37,10 @@ int main(int argc, char **argv){
 					"	\n"<<
 					"	potential = {\n"<<
 					"		is_variable_equal = {\n"<<
-					"			which = country_levy_pcnt\n"<<
+					"			which = country_levy_pcnt_int\n"<<
 					"			value = "<<i<<"\n"<<
 					"		}\n"<<
-					"		has_country_flag = acr_taxes_setup\n"<<
+					"		has_country_flag = acr_setup_national_levies\n"<<
 					"	}\n"<<
 					"	\n"<<
 					"	allow = {}\n"<<
@@ -48,65 +50,20 @@ int main(int argc, char **argv){
 					"			limit = {\n"<<
 					"				ai = yes\n"<<
 					"			}\n"<<
-					"			if = {\n"<<
-					"				limit = {\n"<<
-					"					OR = {\n"<<
-					"						AND = {\n"<<
-					"							is_at_war = yes\n"<<
-					"							AND = {\n"<<
-					"								check_variable = {\n"<<
-					"									which = country_levy_pcnt\n"<<
-					"									value = 10\n"<<
-					"								}\n"<<
-					"							}\n"<<
-					"						}\n"<<
-					"						AND = {\n"<<
-					"							NOT = { is_at_war = yes }\n"<<
-					"							AND = {\n"<<
-					"								check_variable = {\n"<<
-					"									which = country_levy_pcnt\n"<<
-					"									value = 10\n"<<
-					"								}\n"<<
-					"							}\n"<<
-					"						}\n"<<
-					"					}\n"<<
-					"				}\n"<<
-					"				set_variable = {\n"<<
-					"					which = tax_increase_factor\n"<<
-					"					value = -9\n"<<
-					"				}\n"<<
-					"			}\n"<<
-					"			else = {\n"<<
-					"				set_variable = {\n"<<
-					"					which = tax_increase_factor\n"<<
-					"					value = 9\n"<<
-					"				}\n"<<
-					"			}\n"<<
+					"			acr_ai_adjust_country_levy_pcnt = yes\n"<<
 					"		}\n"<<
-					"		update_levies_pcnt_acr_effect = { levy_pcnt = country_levy_pcnt new_pcnt = tax_increase_factor }\n"<<
+					"		update_levies_pcnt_acr_effect = { levy_pcnt = country_levy_pcnt new_pcnt = levies_increase_factor }\n"<<
 					"	}\n"<<
 					"	\n"<<
 					"	ai_will_do = {\n"<<
 					"		factor = 1\n"<<
 					"		modifier = {\n"<<
 					"			factor = 0\n"<<
-					"			AND = { is_at_war = yes }\n"<<
-					"			AND = {\n"<<
-					"				check_variable = {\n"<<
-					"					which = country_levy_pcnt\n"<<
-					"					value = 10\n"<<
-					"				}\n"<<
-					"			}\n"<<
+					"			ai_doesnt_adjust_levies_at_war = yes\n"<<
 					"		}\n"<<
 					"		modifier = {\n"<<
 					"			factor = 0\n"<<
-					"			NOT = { is_at_war = yes }\n"<<
-					"			AND = {\n"<<
-					"				is_variable_equal = {\n"<<
-					"					which = country_levy_pcnt\n"<<
-					"					value = 1\n"<<
-					"				}\n"<<
-					"			}\n"<<
+					"			ai_doesnt_adjust_levies_at_peace = yes\n"<<
 					"		}\n"<<
 					"	}\n"<<
 					"	\n"<<
@@ -118,8 +75,16 @@ int main(int argc, char **argv){
 			_landfm << '\t' << "war_exhaustion_cost = " << war_exhaustion_cost_impact * i << '\n';
 			_landfm << '\t' << "global_unrest = " << unrest_modifier * i << '\n';
 			_landfm << "}\n";
+			_landfm << "purist_levies_level_" << i << " = {\n";
+			_landfm << '\t' << "sailors_recovery_speed = " << purist_manpower_recovery * i << '\n';
+			_landfm << '\t' << "manpower_recovery_speed = " << purist_manpower_recovery * i << '\n';
+			_landfm << '\t' << "war_exhaustion_cost = " << war_exhaustion_cost_impact * i << '\n';
+			_landfm << '\t' << "global_unrest = " << (unrest_modifier * i * 2) << '\n';
+			_landfm << "}\n";
 			_landfml <<" levies_level_" << i << ":0 \"£icon_manpower£ Country levies: "<<i<<"% £icon_sailors£\"\n";
 			_landfml <<" desc_levies_level_" << i << ":0 \"\"\n";
+			_landfml <<" purist_levies_level_" << i << ":0 \"£icon_manpower£ Country levies: "<<i<<"% £icon_sailors£\"\n";
+			_landfml <<" desc_purist_levies_level_" << i << ":0 \"\"\n";
 		}
 	}
 	
